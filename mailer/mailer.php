9a7c035dea39bb90b1994482a3de6e49413d7635
avuillermot <avuillermot@hotmail.com>
2015-08-26 23:29:54 +0200

    bdd config

--- mailer/mailer.php
+++ mailer/mailer.php
@@ -2,7 +2,24 @@
 include("class.phpmailer.php");
 include("class.smtp.php");
 date_default_timezone_set("Europe/Paris");
-header('Location: http://www.lemonde.fr/');
+header('Location: http://www.lequipe.fr/');
+
+$link = mysql_connect("localhost", "uillH_takedocfr", "Cam31122010") or die("Impossible de se connecter : " . mysql_error());
+mysql_select_db('uillhot84013com22148_takedocfr',$link); 
+echo 'Connexion réussie';
+
+$sql = 'SELECT name,value FROM email_contact'; 
+$req = mysql_query($sql) or die('Erreur SQL !<br>'.$sql.'<br>'.mysql_error()); 
+
+while($data = mysql_fetch_assoc($req)) 
+{ 
+	if ($data['name'] == 'USERNAME') $username = $data['value'];
+	if ($data['name'] == 'PASSWORD') $password = $data['value'];
+	if ($data['name'] == 'SUBJECT') $subject = $data['value'];
+	if ($data['name'] == 'FROM') $from = $data['value'];
+	if ($data['name'] == 'FROM_NAME') $fromName = $data['value'];
+}
+mysql_close($link);
  
 $mail             = new PHPMailer();
  
@@ -14,10 +31,10 @@ $mail->SMTPAuth   = true;
 $mail->Host       = "mail.takedoc.fr";  
 $mail->Port       = 25;             
  
-$mail->Username   = "contact@takedoc.fr";
-$mail->Password   = "Kerenann06041979";        
-$mail->From       = "contact@takedoc.fr"; //adresse d’envoi correspondant au login entrée précédement
-$mail->FromName   = "Contact TakeDoc.fr"; // nom qui sera affiché
+$mail->Username   = $username;
+$mail->Password   = $password;        
+$mail->From       = $from; //adresse d’envoi correspondant au login entrée précédement
+$mail->FromName   = $fromName; // nom qui sera affiché
 $mail->Subject    = "Vous avez un nouveau contact TakeDoc.fr :" . $_POST['email']; // sujet
 $mail->AltBody    = "Vous avez un contact"; //Body au format texte
 
